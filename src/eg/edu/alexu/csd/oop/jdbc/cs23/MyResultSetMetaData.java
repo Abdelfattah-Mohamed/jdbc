ec0da2b8d90ae2a2378cb9545e83a6858de0135c
Abdelfattah Mohamed <abdelftahmohamed30@gmail.com>
2018-12-08 17:19:37 +0200

    fix jar

--- src/eg/edu/alexu/csd/oop/jdbc/cs23/MyResultSetMetaData.java
+++ src/eg/edu/alexu/csd/oop/jdbc/cs23/MyResultSetMetaData.java
@@ -2,33 +2,51 @@ package eg.edu.alexu.csd.oop.jdbc.cs23;
 
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
+import java.util.Arrays;
+import java.util.List;
+
+import eg.edu.alexu.csd.oop.db.cs04.XML.DTDGenerator;
 
 public class MyResultSetMetaData implements ResultSetMetaData {
 
+	private MyResultset rs = new MyResultset();
+	private Object[][] result;
+	private String[] columnNames;
+	private Statement statement;
+
+	public MyResultSetMetaData(MyResultset myResultset, Object[][] result, String[] columnNames, Statement statement) {
+		rs = myResultset;
+		this.result = result;
+		this.columnNames = columnNames;
+		this.statement = statement;
+	}
+
 	@Override
 	public boolean isWrapperFor(Class<?> iface) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
 	public <T> T unwrap(Class<T> iface) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
 	public String getCatalogName(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
 	public String getColumnClassName(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 
 	}
@@ -36,58 +54,67 @@ public class MyResultSetMetaData implements ResultSetMetaData {
 	@Override
 	public int getColumnCount() throws SQLException {
 		// TODO Auto-generated method stub
-		return 0;
+
+		return result[0].length;
 	}
 
 	@Override
 	public int getColumnDisplaySize(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
 	public String getColumnLabel(int arg0) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+
+		return columnNames[rs.getInt(arg0)];
 	}
 
 	@Override
 	public String getColumnName(int arg0) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+
+		return columnNames[rs.getInt(arg0)];
 	}
 
 	@Override
 	public int getColumnType(int arg0) throws SQLException {
-		// TODO Auto-generated method stub
+		return colType(result, arg0);
+	}
+
+	private static int colType(Object[][] given, int colNo) {
+		if ((given[1][colNo].toString().contains("\'"))) {
+			return Types.VARCHAR;
+		} else if (!(given[1][colNo].toString().contains("\'"))) {
+			return Types.INTEGER;
+		}
 		return 0;
 	}
 
 	@Override
 	public String getColumnTypeName(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
 	public int getPrecision(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
 	public int getScale(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
 	public String getSchemaName(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 
 	}
@@ -95,68 +122,69 @@ public class MyResultSetMetaData implements ResultSetMetaData {
 	@Override
 	public String getTableName(int arg0) throws SQLException {
 		// TODO Auto-generated method stub
+		/* After edit the dbms interface */
 		return null;
 	}
 
 	@Override
 	public boolean isAutoIncrement(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
 	public boolean isCaseSensitive(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
 	public boolean isCurrency(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
 	public boolean isDefinitelyWritable(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
 	public int isNullable(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
 	public boolean isReadOnly(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
 	public boolean isSearchable(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
 	public boolean isSigned(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
 	public boolean isWritable(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 
 	}
--- src/eg/edu/alexu/csd/oop/jdbc/cs23/MyStatement.java
+++ src/eg/edu/alexu/csd/oop/jdbc/cs23/MyStatement.java
@@ -2,6 +2,8 @@ package eg.edu.alexu.csd.oop.jdbc.cs23;
 
 import eg.edu.alexu.csd.oop.db.Database;
 import eg.edu.alexu.csd.oop.db.cs04.MyDatabase;
+import eg.edu.alexu.csd.oop.db.cs04.XML.DTDGenerator;
+import eg.edu.alexu.csd.oop.db.cs04.XML.SQLOrder;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -10,326 +12,346 @@ import java.sql.SQLWarning;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-
 public class MyStatement implements Statement {
-	
 
-    private ArrayList<String> batch = new ArrayList<>();
-    //TODO lazem n3ml l depenency injection hena ,,
-    private Database database ;
-    private Connection connection;
-    private ResultSet currentResultSet;
-    private String path;
-    private int timeLimit;
-// TODO hn3delha bel path walla la2
-    private boolean closed;
- // TODO m7tagen n3del 7war l 2 constructors da lama nersa 3ala tre2a
-    MyStatement(Connection connection){
-        this.connection=connection;
-    }
+	private ArrayList<String> batch = new ArrayList<>();
+	// TODO lazem n3ml l depenency injection hena ,,
+	private Database database;
+	private Connection connection;
+	private ResultSet currentResultSet;
+	private String path;
+	private int timeLimit;
+	// TODO hn3delha bel path walla la2
+	private boolean closed;
 
-    MyStatement(Connection connection,String path,Database mdb){
-        this.connection=connection;
-        this.path=path;
-        this.database=mdb;
-    }
+	// TODO m7tagen n3del 7war l 2 constructors da lama nersa 3ala tre2a
+	MyStatement(Connection connection) {
+		this.connection = connection;
+	}
 
-    @Override
+	MyStatement(Connection connection, String path, Database mdb) {
+		this.connection = connection;
+		this.path = path;
+		this.database = mdb;
+	}
+
+	@Override
 	public boolean isWrapperFor(Class<?> iface) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public <T> T unwrap(Class<T> iface) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void addBatch(String arg0) throws SQLException {
-		 if (closed) {
-	            throw new SQLException("The statement has been closed.");
-	        }
+		if (closed) {
+			throw new SQLException("The statement has been closed.");
+		}
 		batch.add(arg0);
 
 	}
 
 	@Override
 	public void cancel() throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void clearBatch() throws SQLException {
-        if (closed) {
-            throw new SQLException("The statement has been closed.");
-        }
-        batch=new ArrayList<>();
+		if (closed) {
+			throw new SQLException("The statement has been closed.");
+		}
+		batch = new ArrayList<>();
 
 	}
 
 	@Override
 	public void clearWarnings() throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void close() throws SQLException {
 		// TODO ?
-        this.closed=true;
+		this.closed = true;
 	}
 
 	@Override
 	public void closeOnCompletion() throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public boolean execute(String arg0) throws SQLException {
-        if(!closed) {
-        	PathParser pp = new PathParser();
-        	arg0 = pp.Parser(arg0, this.path);
-            return database.executeStructureQuery(arg0);
-        }throw new SQLException("The statement has been closed.");
+		if (!closed) {
+			PathParser pp = new PathParser();
+			arg0 = pp.Parser(arg0, this.path);
+			String[] str = arg0.split(" ");
+			if (str[0].toLowerCase().equals("insert") || str[0].toLowerCase().equals("update")
+					|| str[0].toLowerCase().equals("delete")) {
+				int x = executeUpdate(arg0);
+				if (x > 0) {
+					return true;
+				} else {
+					return false;
+				}
+			} else if (str[0].toLowerCase().equals("select")) {
+				if (executeQuery(arg0).next() || executeQuery(arg0).previous()) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+			return database.executeStructureQuery(arg0);
+		}
+		throw new SQLException("The statement has been closed.");
 	}
 
 	@Override
 	public boolean execute(String arg0, int arg1) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public boolean execute(String arg0, int[] arg1) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public boolean execute(String arg0, String[] arg1) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public int[] executeBatch() throws SQLException {
 		Splitter split = new Splitter();
-		int num =0;
-        if (closed) {
-            throw new SQLException("The statement has been closed.");
-        }
-        int[] updates = new int[batch.size()];
-        int i = 0;
-        for (final String batch : batch) {
-        	num = split.QuerySplitter(batch);
-            if (num==2){//it's not create nor drop) {
-                updates[i] = executeUpdate(batch);
-            } else if(num == 1){
-            	// TODO mmkn a5leh yrg3 0 l true w arg3 -1 lw false 34an atb3 ? 
-            	execute(batch);
-                updates[i] = 0;
-            }else if(num == 3){
-            	// TODO elmfrod a5lyh ytb3 l 7agat y3ny walla eh ?
-            	executeQuery(batch);
-                updates[i] = 0;
-            }
-            i++;
-        }
-        return updates;
+		int num = 0;
+		if (closed) {
+			throw new SQLException("The statement has been closed.");
+		}
+		int[] updates = new int[batch.size()];
+		int i = 0;
+		for (final String batch : batch) {
+			num = split.QuerySplitter(batch);
+			if (num == 2) {// it's not create nor drop) {
+				updates[i] = executeUpdate(batch);
+			} else if (num == 1) {
+				// TODO mmkn a5leh yrg3 0 l true w arg3 -1 lw false 34an atb3 ?
+				execute(batch);
+				updates[i] = 0;
+			} else if (num == 3) {
+				// TODO elmfrod a5lyh ytb3 l 7agat y3ny walla eh ?
+				executeQuery(batch);
+				updates[i] = 0;
+			}
+			i++;
+		}
+		return updates;
 
 	}
 
 	@Override
 	public ResultSet executeQuery(String arg0) throws SQLException {
-		 //if(!closed){
-		//throw ?
-		//}
-        Object[][] table = database.executeQuery(arg0);
-        String columns=  database.executeQuery(arg0)[0].toString();
-        //String tableName = database.
-        //currentResultSet =new Resultset(table,columns,tableName,this);
-	
-        return null;
+		// if(!closed){
+		// throw ?
+		// }
+		Object[][] table = database.executeQuery(arg0);
+		String x = SQLOrder.getInstance().getTable_head();
+		x = x.replace(".xml", ".dtd");
+		String[] strings = DTDGenerator.getDTDColumns(x);
+		// String tableName = database.
+		// currentResultSet =new Resultset(table,columns,tableName,this);
+
+		MyResultset rs = new MyResultset();
+		rs.MyResultSet(table, this, strings);
+		return rs;
 	}
 
 	@Override
 	public int executeUpdate(String arg0) throws SQLException {
-        if(!closed){
-            return database.executeUpdateQuery(arg0);
-        }
+		if (!closed) {
+			return database.executeUpdateQuery(arg0);
+		}
 
-        throw new SQLException("The statement has been closed.");
-    }
+		throw new SQLException("The statement has been closed.");
+	}
 
 	@Override
 	public int executeUpdate(String arg0, int arg1) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public int executeUpdate(String arg0, int[] arg1) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public int executeUpdate(String arg0, String[] arg1) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Connection getConnection() throws SQLException {
-		
+
 		return this.connection;
 	}
 
 	@Override
 	public int getFetchDirection() throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public int getFetchSize() throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public ResultSet getGeneratedKeys() throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public int getMaxFieldSize() throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public int getMaxRows() throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public boolean getMoreResults() throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public boolean getMoreResults(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public int getQueryTimeout() throws SQLException {
-		
+
 		return timeLimit;
 	}
 
 	@Override
 	public ResultSet getResultSet() throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public int getResultSetConcurrency() throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public int getResultSetHoldability() throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public int getResultSetType() throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public int getUpdateCount() throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public SQLWarning getWarnings() throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public boolean isCloseOnCompletion() throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public boolean isClosed() throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public boolean isPoolable() throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setCursorName(String arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setEscapeProcessing(boolean arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setFetchDirection(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setFetchSize(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setMaxFieldSize(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setMaxRows(int arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setPoolable(boolean arg0) throws SQLException {
-		
+
 		throw new UnsupportedOperationException();
 	}
 
